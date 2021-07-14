560cfa2769b914332cfa416109c505859ab9ddac
manulera <manulera@users.noreply.github.com>
2021-07-14 15:58:16 +0000

    deploy: manulera/GenestorianLandingPageSource@9723a8056bcbdeaad12d90222ad67379a3f4bdbd

---
+++ html/web_interface/precache-manifest.5ad350d55f6868ff5f068e39818f43d9.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "313de58e266515ce7a9810fa91ddeae3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a176b12861cc650c90e8",
+    "url": "/static/css/2.2c0dd2fd.chunk.css"
+  },
+  {
+    "revision": "9b7f1064a3f45a2eebb2",
+    "url": "/static/css/main.b4fa84e1.chunk.css"
+  },
+  {
+    "revision": "a176b12861cc650c90e8",
+    "url": "/static/js/2.eb930cf9.chunk.js"
+  },
+  {
+    "revision": "2faee4ab06820f52e4efeeb0858cd8f0",
+    "url": "/static/js/2.eb930cf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b7f1064a3f45a2eebb2",
+    "url": "/static/js/main.62d6b074.chunk.js"
+  },
+  {
+    "revision": "15360ff1214742c304c2",
+    "url": "/static/js/runtime-main.4a783144.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/static/media/icons-20.cef8cdbb.woff"
+  },
+  {
+    "revision": "9937c52ecf1224033b83886cbfb10ae6",
+    "url": "/static/media/ubuntu-mono-latin-400.9937c52e.woff2"
+  },
+  {
+    "revision": "fda3eddd27016d48d3ca4225d1e7df18",
+    "url": "/static/media/ubuntu-mono-latin-400.fda3eddd.woff"
+  },
+  {
+    "revision": "4cbe65be70f6363aaa1e7e6dddfb762c",
+    "url": "/static/media/ubuntu-mono-latin-400italic.4cbe65be.woff2"
+  },
+  {
+    "revision": "c2cb57e65c6d836fa77d83bb183b23d1",
+    "url": "/static/media/ubuntu-mono-latin-400italic.c2cb57e6.woff"
+  },
+  {
+    "revision": "15a5b8c85d5080819335d2164e843e31",
+    "url": "/static/media/ubuntu-mono-latin-700.15a5b8c8.woff"
+  },
+  {
+    "revision": "35243340e820a1bc839372337634ee5f",
+    "url": "/static/media/ubuntu-mono-latin-700.35243340.woff2"
+  },
+  {
+    "revision": "123009e43c0207783a9d8881049c12f9",
+    "url": "/static/media/ubuntu-mono-latin-700italic.123009e4.woff"
+  },
+  {
+    "revision": "b58ee8b30eea684d1e50844378f84ed1",
+    "url": "/static/media/ubuntu-mono-latin-700italic.b58ee8b3.woff2"
+  }
+]);
